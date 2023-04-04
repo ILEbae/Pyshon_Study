f2fb05db8d3a5bc87a9f4a2b078e87477fd537f0
seungjun Bae <wnstmdqo94@naver.com>
2023-04-05 02:41:37 +0900

    A+B_7 문제

--- BaekJoon/A+B_7.py
+++ BaekJoon/A+B_7.py
@@ -1,9 +1,16 @@
-T = input()
+"""T = input()
 C=[]
 
 for _ in range(int(T)):
     A,B = map(int ,input().split())
     C.append(A+B) 
 for i in range(int(T)):
-    print("Case#",i+1,": ",C[i])
+    print("Case #{}: {} + {} = {}".format(i+1, A, B, C[i]))
+"""
+
+t = int(input())
+
+for i in range(1, t+1):  # 1부터 t까지
+    a, b = map(int, input().split())
+    print(f'Case #{i}: {a+b}')
 
